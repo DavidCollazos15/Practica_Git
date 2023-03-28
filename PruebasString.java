54d677017bf99b16b2e0d59b4f5594ceca1cc6c3
DavidCollazos15 <115153692+DavidCollazos15@users.noreply.github.com>
2023-03-28 10:45:01 +0200

    Update PruebasString.java
    Añadi un String de "Ejemplo entornos de desarrollo"

--- PruebasString.java
+++ PruebasString.java
@@ -20,7 +20,7 @@ public class PruebasString {
         System.out.println("mostrar la inicial de mi apellido: "+apellido.charAt(0));
         System.out.println("el numero de caracteres de "+nombre+" es: "+nombre.length());
 
-
+        System.out.println("Ejemplo entornos de desarrollo");
 
 
     }
